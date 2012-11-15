d5572f623f3bfe78e1b7506f466ea3216416bb72
Mark Ryall <mark@ryall.name>
2012-11-16 00:23:08 +1100

    made credentials optional for cctray

--- lib/chicanery/cctray.rb
+++ lib/chicanery/cctray.rb
@@ -6,13 +6,13 @@ module Chicanery
   class Cctray
     attr_reader :name, :uri, :user, :password
 
-    def initialize name, url, user, password
+    def initialize name, url, user=nil, password=nil
       @name, @uri, @user, @password = name, URI(url), user, password
     end
 
     def get
       req = Net::HTTP::Get.new(uri.path)
-      req.basic_auth user, password
+      req.basic_auth user, password if user and password
       res = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https', verify_mode: OpenSSL::SSL::VERIFY_NONE) do |https|
         https.request(req)
       end
