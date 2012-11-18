b4bae739ee646b95cc30cee7c7c13b8e946bdc29
Mark Ryall <mark@ryall.name>
2012-11-18 14:05:01 +1000

    added a growl notification to example

--- examples/travis.rb
+++ examples/travis.rb
@@ -2,25 +2,29 @@ require 'chicanery/cctray'
 
 server Chicanery::Cctray.new 'travis', 'https://api.travis-ci.org/repositories/markryall/chicanery/cc.xml'
 
+def growlnotify message
+  `growlnotify -t "some new chicanery ..." --image ~/icons/chicanery.png -m \"#{message}\"`
+end
+
 when_started do |job_name, job|
   `afplay ~/build_sounds/ticktock.mp3`
-  puts "job #{job_name} has started"
+  growlnotify "job #{job_name} has started"
 end
 
 when_succeeded do |job_name, job|
-  puts "job #{job_name} #{job[:last_label]} has succeeded"
+  growlnotify "job #{job_name} #{job[:last_label]} has succeeded"
 end
 
 when_failed do |job_name, job|
-  puts "job #{job_name} #{job[:last_label]} has failed"
+  growlnotify "job #{job_name} #{job[:last_label]} has failed"
 end
 
 when_broken do |job_name, job|
   `afplay ~/build_sounds/ambulance.mp3`
-  puts "job #{job_name} is broken"
+  growlnotify "job #{job_name} is broken"
 end
 
 when_fixed do |job_name, job|
   `afplay ~/build_sounds/applause.mp3`
-  puts "job #{job_name} is fixed"
+  growlnotify "job #{job_name} is fixed"
 end
