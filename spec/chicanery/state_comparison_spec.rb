f3593e0befc3f6a57ec4b89fc038be640d86c84d
Mark Ryall <mark@ryall.name>
2012-11-18 15:15:06 +1000

    added specs for started events

--- spec/chicanery/state_comparison_spec.rb
+++ spec/chicanery/state_comparison_spec.rb
@@ -1,20 +1,37 @@
 describe Chicanery::StateComparison do
   include Chicanery::StateComparison
 
-  let(:current_jobs) { {} }
-  let(:previous_jobs) { {} }
-
   describe '#compare_jobs' do
+    let(:current_jobs) { {} }
+    let(:previous_jobs) { {} }
+
+    after { compare_jobs current_jobs, previous_jobs }
+
     it 'should do nothing when there are no jobs in current state' do
       should_not_receive :compare_job
       previous_jobs[:job] = {}
-      compare_jobs current_jobs, previous_jobs
     end
 
     it 'should do nothing when there is no previous state' do
       should_not_receive :compare_job
       current_jobs[:job] = {}
-      compare_jobs current_jobs, previous_jobs
+    end
+  end
+
+  describe '#compare_job' do
+    let(:current_job) { {last_build_time: Time.now } }
+    let(:previous_job) { {last_build_time: (Time.now-1) } }
+
+    after { compare_job 'name', current_job, previous_job }
+
+    it 'should do nothing when build times are equal' do
+      current_job[:last_build_time] = previous_job[:last_build_time] = Time.now
+    end
+
+    it 'should notify started_handlers when activity is now building when it was previously sleeping' do
+      current_job[:activity] = :building
+      previous_job[:activity] = :sleeping
+      should_receive(:notify_started_handlers).with 'name', current_job
     end
   end
 end
