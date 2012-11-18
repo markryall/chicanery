3b9b66413b2852e17caafc5e6724bfe22a039003
Mark Ryall <mark@ryall.name>
2012-11-18 13:48:40 +1000

    added interrupt handler to allow a clean exit and to be able to test looping behaviour

--- lib/chicanery.rb
+++ lib/chicanery.rb
@@ -12,22 +12,25 @@ module Chicanery
   VERSION = "0.0.4"
 
   def execute *args
-    load args.shift
-    poll_period = args.shift
-    loop do
-      previous_state = restore
-      current_state = {
-        servers: {}
-      }
-      servers.each do |server|
-        current_jobs = server.jobs
-        compare_jobs current_jobs, previous_state[:servers][server.name] if previous_state[:servers]
-        current_state[:servers][server.name] = current_jobs
+    begin
+      load args.shift
+      poll_period = args.shift
+      loop do
+        previous_state = restore
+        current_state = {
+          servers: {}
+        }
+        servers.each do |server|
+          current_jobs = server.jobs
+          compare_jobs current_jobs, previous_state[:servers][server.name] if previous_state[:servers]
+          current_state[:servers][server.name] = current_jobs
+        end
+        run_handlers.each {|handler| handler.call current_state }
+        persist current_state
+        break unless poll_period
+        sleep poll_period.to_i
       end
-      run_handlers.each {|handler| handler.call current_state }
-      persist current_state
-      break unless poll_period
-      sleep poll_period.to_i
+    rescue Interrupt
     end
   end
 end
--- spec/chicanery_spec.rb
+++ spec/chicanery_spec.rb
@@ -4,9 +4,16 @@ describe Chicanery do
   include Chicanery
 
   describe '#execute' do
+    before { stub! :load }
+
     it 'should load configuration and exit immediately when nothing is configured no poll period is provided' do
       should_receive(:load).with 'configuration'
       execute 'configuration'
+    end
+
+    it 'should load configuration and exit immediately when nothing is configured no poll period is provided' do
+      should_receive(:sleep).with(10).and_raise Interrupt
+      execute 'configuration', '10'
     end
   end
 end
