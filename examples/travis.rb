9c51afab2aea19caaafbc249b3264ce344b7f4cd
Mark Ryall <mark@ryall.name>
2012-11-18 13:37:07 +1000

    a few small changes to the example

--- examples/travis.rb
+++ examples/travis.rb
@@ -3,23 +3,24 @@ require 'chicanery/cctray'
 server Chicanery::Cctray.new 'travis', 'https://api.travis-ci.org/repositories/markryall/chicanery/cc.xml'
 
 when_started do |job_name, job|
-  puts "#{job_name} build has started"
+  `afplay ~/build_sounds/startup.mp3`
+  puts "job #{job_name} has started"
 end
 
 when_succeeded do |job_name, job|
-  puts "#{job_name} has succeeded"
+  puts "job #{job_name} #{job[:last_label]} has succeeded"
 end
 
 when_failed do |job_name, job|
-  puts "#{job_name} has failed"
+  puts "job #{job_name} #{job[:last_label]} has failed"
 end
 
 when_broken do |job_name, job|
   `afplay ~/build_sounds/ambulance.mp3`
-  puts "#{job_name} is broken"
+  puts "job #{job_name} is broken"
 end
 
 when_fixed do |job_name, job|
   `afplay ~/build_sounds/applause.mp3`
-  puts "#{job_name} is fixed"
+  puts "job #{job_name} is fixed"
 end
