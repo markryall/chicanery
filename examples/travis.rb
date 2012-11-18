cddf6bbe5baa980de6de32a175cd5a784b75402a
Mark Ryall <mark@ryall.name>
2012-11-18 16:28:17 +1000

    changed order of growl notification and afplay in example

--- examples/travis.rb
+++ examples/travis.rb
@@ -7,8 +7,8 @@ def growlnotify message
 end
 
 when_started do |job_name, job|
-  `afplay ~/build_sounds/ticktock.mp3`
   growlnotify "job #{job_name} has started"
+  `afplay ~/build_sounds/ticktock.mp3`
 end
 
 when_succeeded do |job_name, job|
@@ -20,11 +20,11 @@ when_failed do |job_name, job|
 end
 
 when_broken do |job_name, job|
-  `afplay ~/build_sounds/ambulance.mp3`
   growlnotify "job #{job_name} is broken"
+  `afplay ~/build_sounds/ambulance.mp3`
 end
 
 when_fixed do |job_name, job|
-  `afplay ~/build_sounds/applause.mp3`
   growlnotify "job #{job_name} is fixed"
+  `afplay ~/build_sounds/applause.mp3`
 end
