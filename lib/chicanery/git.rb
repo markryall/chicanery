f5d5eed65b85c7a3e4a4f8b30670bd97b6316f78
Mark Ryall <mark@ryall.name>
2012-11-24 13:46:29 +1000

    changed git poller to skip checkout and merge

--- lib/chicanery/git.rb
+++ lib/chicanery/git.rb
@@ -12,12 +12,12 @@ module Chicanery
       FileUtils.mkdir 'repos' unless File.exists? 'repos'
 
       Dir.chdir('repos') do
-        `git clone -q #{@url} #{name}` unless File.exists? name
+        `git clone -q -n #{@url} #{name}` unless File.exists? name
       end
 
       Dir.chdir("repos/#{name}") do
-        `git pull -q`
-        `git log -n 1 --oneline` =~ /^([^ ]*) /
+        `git fetch -q origin`
+        `git log -n 1 origin/master --oneline` =~ /^([^ ]*) /
         $1
       end
     end
