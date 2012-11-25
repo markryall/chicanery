4e1c72f5867a87d32f1ce872e4614f5dd2620688
Mark Ryall <mark@ryall.name>
2012-11-25 21:09:47 +1000

    added a more convenient method to configure a git repository

--- lib/chicanery/git.rb
+++ lib/chicanery/git.rb
@@ -1,34 +1,40 @@
 require 'fileutils'
 
 module Chicanery
-  class Git
-    attr_reader :name, :url
+  module Git
+    class Repo
+      attr_reader :name, :url
 
-    def initialize params
-      @name, @remotes = params[:name], params[:remotes]
-      @remotes['origin'] = { url: params[:url], branches: params[:branches] }
+      def initialize params
+        @name, @remotes = params[:name], params[:remotes]
+        @remotes['origin'] = { url: params[:url], branches: params[:branches] }
+      end
+
+      def state
+        FileUtils.mkdir 'repos' unless File.exists? 'repos'
+
+        Dir.chdir('repos') do
+          `git clone -q -n #{@url} #{name}` unless File.exists? name
+        end
+
+        remotes = {}
+        Dir.chdir("repos/#{name}") do
+          @remotes.each do |name, remote|
+            remotes[name] = {}
+            `git remote add #{name} #{remote[:url]}` unless `git remote | grep #{name}`.chomp == name
+            `git fetch -q #{name}`
+            (remote[:branches] || ['master']).each do |branch|
+              `git log -n 1 #{name}/#{branch} --oneline` =~ /^([^ ]*) /
+              remotes[name][branch] = $1
+            end
+         end
+        end
+        remotes
+      end
     end
 
-    def state
-      FileUtils.mkdir 'repos' unless File.exists? 'repos'
-
-      Dir.chdir('repos') do
-        `git clone -q -n #{@url} #{name}` unless File.exists? name
-      end
-
-      remotes = {}
-      Dir.chdir("repos/#{name}") do
-        @remotes.each do |name, remote|
-          remotes[name] = {}
-          `git remote add #{name} #{remote[:url]}` unless `git remote | grep #{name}`.chomp == name
-          `git fetch -q #{name}`
-          (remote[:branches] || ['master']).each do |branch|
-            `git log -n 1 #{name}/#{branch} --oneline` =~ /^([^ ]*) /
-            remotes[name][branch] = $1
-          end
-       end
-      end
-      remotes
+    def git_repo params
+      repo Repo.new params
     end
   end
 end
