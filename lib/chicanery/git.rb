86424de7580b207a45399923101861d7fcf7f23b
Mark Ryall <mark@ryall.name>
2012-11-28 23:24:31 +1100

    fixed naming collision with remotes attribute

--- lib/chicanery/git.rb
+++ lib/chicanery/git.rb
@@ -15,22 +15,26 @@ module Chicanery
         FileUtils.mkdir 'repos' unless File.exists? 'repos'
 
         Dir.chdir('repos') do
-          `git clone -q -n #{remotes['origin'][:url]} #{name}` unless File.exists? name
+          git "clone -q -n #{remotes['origin'][:url]} #{name}" unless File.exists? name
         end
 
-        remotes = {}
+        remotes_status = {}
         Dir.chdir("repos/#{name}") do
           remotes.each do |name, remote|
-            remotes[name] = {}
-            `git remote add #{name} #{remote[:url]}` unless `git remote | grep #{name}`.chomp == name
-            `git fetch -q #{name}`
+            remotes_status[name] = {}
+            git "remote add #{name} #{remote[:url]}" unless git("remote | grep #{name}") == name
+            git "fetch -q #{name}"
             (remote[:branches] || ['master']).each do |branch|
-              `git log -n 1 #{name}/#{branch} --oneline` =~ /^([^ ]*) /
-              remotes[name][branch] = $1
+              git("log -n 1 #{name}/#{branch} --oneline") =~ /^([^ ]*) /
+              remotes_status[name][branch] = $1
             end
          end
         end
-        remotes
+        remotes_status
+      end
+
+      def git command
+        `git #{command}`.chomp
       end
     end
 
