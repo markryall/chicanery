5b43b546daf5d6cee6cc2db0d94dcb60639247ae
Mark Ryall <mark@ryall.name>
2012-12-05 22:16:28 +1000

    added example script to revert all uncommitted changes after a specified number of seconds

---
+++ examples/evil.rb
@@ -0,0 +1,22 @@
+require 'chicanery/git'
+
+include Chicanery::Git
+
+git_repo 'chicanery', '.', branches: [:master]
+
+start = Time.now.to_i
+maximum = ARGV.shift.to_i
+
+when_run do |state|
+  elapsed = Time.now.to_i - start
+  if elapsed >= maximum
+    puts "no commits detected in #{elapsed} seconds - reverting all uncommitted changes"
+    `git reset --hard`
+    start = Time.now.to_i
+  end
+end
+
+when_commit do |branch, commit, previous|
+  puts "commit detected - resetting timer"
+  start = Time.now.to_i
+end
