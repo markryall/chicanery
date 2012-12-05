c544b40918b46799c02962d109c5f81568fe9fc9
Mark Ryall <mark@ryall.name>
2012-12-05 23:51:21 +1000

    added polling github remote and local repo to example

--- examples/github.rb
+++ examples/github.rb
@@ -2,7 +2,9 @@ require 'chicanery/git'
 
 include Chicanery::Git
 
-git_repo 'chicanery', '.', branches: [:master]
+git_repo 'chicanery', '.', branches: [:master], remotes: {
+  github: { url: 'git://github.com/markryall/chicanery.git' }
+}
 
 when_commit do |branch, commit, previous|
   puts "commit #{previous}..#{commit} detected in #{branch}"
