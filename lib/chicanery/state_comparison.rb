3e8cba6b666cd602e2d4929215d1400f2e24c972
Mark Ryall <mark@ryall.name>
2012-12-05 21:57:35 +1000

    added check for any previous repo state

--- lib/chicanery/state_comparison.rb
+++ lib/chicanery/state_comparison.rb
@@ -20,6 +20,7 @@ module Chicanery
     end
 
     def compare_repo_state name, current, previous
+      return unless previous
       current.each do |branch, state|
         next unless previous[branch]
         notify_commit_handlers "#{name}/#{branch}", state, previous[branch] if state != previous[branch]
