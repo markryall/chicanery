4a40c0f944d90931e032ccfda31d6288c78a789b
Mark Ryall <mark@ryall.name>
2012-12-09 19:31:53 +1000

    added check for modifications (to reset timer)

--- examples/evil.rb
+++ examples/evil.rb
@@ -4,23 +4,28 @@ include Chicanery::Git
 
 git_repo 'chicanery', '.', branches: [:master]
 
-start = Time.now.to_i
+def now
+  Time.now.to_i
+end
+
+start = now
 maximum = ARGV.shift.to_i
 
 puts "Uncommitted changes will be automatically reverted after #{maximum} seconds"
 
 when_run do |state|
-  elapsed = Time.now.to_i - start
+  start = now if `git status`.chomp.split("\n").last == 'nothing to commit, working directory clean'
+  elapsed = now - start
   if elapsed >= maximum
     puts "no commits detected in #{elapsed} seconds - reverting all uncommitted changes"
     `afplay ~/build_sounds/ticktock.mp3`
     `git reset --hard`
     `git clean -d -x -f`
-    start = Time.now.to_i
+    start = now
   end
 end
 
 when_commit do |branch, commit, previous|
   puts "commit detected - resetting timer"
-  start = Time.now.to_i
+  start = now
 end
