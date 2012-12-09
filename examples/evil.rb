ec789571a4b43346af6640580e693362758bddd5
Mark Ryall <mark@ryall.name>
2012-12-09 19:34:47 +1000

    added growl notifications

--- examples/evil.rb
+++ examples/evil.rb
@@ -8,16 +8,20 @@ def now
   Time.now.to_i
 end
 
+def say message
+  `growlnotify -t "some new chicanery ..." --image ~/icons/chicanery.png -m \"#{message}\"`
+end
+
 start = now
 maximum = ARGV.shift.to_i
 
-puts "Uncommitted changes will be automatically reverted after #{maximum} seconds"
+say "Uncommitted changes will be automatically reverted after #{maximum} seconds"
 
 when_run do |state|
   start = now if `git status`.chomp.split("\n").last == 'nothing to commit, working directory clean'
   elapsed = now - start
   if elapsed >= maximum
-    puts "no commits detected in #{elapsed} seconds - reverting all uncommitted changes"
+    say "no commits detected in #{elapsed} seconds - reverting all uncommitted changes"
     `afplay ~/build_sounds/ticktock.mp3`
     `git reset --hard`
     `git clean -d -x -f`
@@ -26,6 +30,6 @@ when_run do |state|
 end
 
 when_commit do |branch, commit, previous|
-  puts "commit detected - resetting timer"
+  say "commit detected - resetting timer"
   start = now
 end
