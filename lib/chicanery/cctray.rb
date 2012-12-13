01bf17e78c79ec956977cb7f4f1bfa8dc66201e2
Mark Ryall <mark@ryall.name>
2012-12-14 00:04:15 +1100

    added function to parse build status (hopefully this will not radically change between build servers)

--- lib/chicanery/cctray.rb
+++ lib/chicanery/cctray.rb
@@ -22,10 +22,9 @@ module Chicanery
     def jobs
       jobs = {}
       Nokogiri::XML(get).css("Project").each do |project|
-        p project[:lastBuildStatus]
         job = {
           activity: project[:activity] == 'Sleeping' ? :sleeping : :building,
-          last_build_status: project[:lastBuildStatus] == 'Success' ? :success : :failure,
+          last_build_status: parse_build_status(project[:lastBuildStatus]),
           last_build_time: project[:lastBuildTime].empty? ? nil : DateTime.parse(project[:lastBuildTime]).to_time.to_i,
           url: project[:webUrl],
           last_label: project[:lastBuildLabel]
@@ -33,6 +32,14 @@ module Chicanery
         jobs[project[:name]] = job unless filtered project[:name]
       end
       jobs
+    end
+
+    def parse_build_status status
+      case status
+      when /$Success/ then :success
+      when /$Unknown/ then :unknown
+      else :failure
+      end
     end
 
     def filtered name
