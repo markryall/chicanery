a20b98e7dc144b3876e11f44d1d2924ab0ac7bc0
Mark Ryall <mark@ryall.name>
2012-12-15 23:24:49 +1000

    added mechanism to configure location of persisted state file

--- lib/chicanery/persistence.rb
+++ lib/chicanery/persistence.rb
@@ -3,14 +3,19 @@ require 'yaml'
 module Chicanery
   module Persistence
     def persist state
-      File.open 'state', 'w' do |file|
+      File.open persist_state_to, 'w' do |file|
         file.puts state.to_yaml
       end
     end
 
     def restore
-      return {} unless File.exist? 'state'
-      YAML.load_file 'state'
+      return {} unless File.exist? persist_state_to
+      YAML.load_file persist_state_to
+    end
+
+    def persist_state_to path=nil
+      @state = path if path
+      @state || 'state'
     end
   end
 end
--- spec/chicanery/persistence_spec.rb
+++ spec/chicanery/persistence_spec.rb
@@ -1,11 +1,19 @@
 describe Chicanery::Persistence do
   include Chicanery::Persistence
 
+  let(:file) { stub 'file' }
+  let(:state) { stub 'state', to_yaml: :yaml }
+
   describe '#persist' do
     it 'should write state to disk as yaml' do
-      file = stub 'file'
-      state = stub 'state', to_yaml: :yaml
       File.should_receive(:open).with('state', 'w').and_yield file
+      file.should_receive(:puts).with :yaml
+      persist state
+    end
+
+    it 'should persist state to where it is told' do
+      persist_state_to 'foo'
+      File.should_receive(:open).with('foo', 'w').and_yield file
       file.should_receive(:puts).with :yaml
       persist state
     end
@@ -18,9 +26,15 @@ describe Chicanery::Persistence do
     end
 
     it 'should read yaml from disk' do
-      state = stub 'state'
       File.should_receive(:exist?).with('state').and_return true
       YAML.should_receive(:load_file).with('state').and_return state
+      restore.should == state
+    end
+
+    it 'should read yaml from another location if it is told to do so' do
+      persist_state_to 'foo'
+      File.should_receive(:exist?).with('foo').and_return true
+      YAML.should_receive(:load_file).with('foo').and_return state
       restore.should == state
     end
   end
