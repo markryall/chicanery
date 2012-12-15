298c4394379ac41c5568feb8005441eb5f5d0fcd
Mark Ryall <mark@ryall.name>
2012-12-15 23:15:12 +1000

    refactored chicanery spec a little

--- spec/chicanery_spec.rb
+++ spec/chicanery_spec.rb
@@ -4,14 +4,14 @@ describe Chicanery do
   describe '#execute' do
     before { %w{load restore persist}.each {|m| stub! m } }
 
+    after { execute ['configuration'] }
+
     it 'should load configuration and exit immediately when nothing is configured no poll period is provided' do
       should_receive(:load).with 'configuration'
-      execute %w{configuration}
     end
 
     it 'should restore previous state' do
       should_receive(:restore)
-      execute %w{configuration}
     end
 
     it 'should persist new state' do
@@ -19,13 +19,11 @@ describe Chicanery do
         servers: {},
         repos: {}
       })
-      execute %w{configuration}
     end
 
     it 'should sleep for specified time when poll period is provided' do
       should_receive(:sleep).with(10).and_raise Interrupt
       poll_period 10
-      execute %w{configuration}
     end
   end
 end
