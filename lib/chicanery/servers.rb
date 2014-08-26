d68ad0810ca6c4773a750f50ded37680e1daeda9
Mark Ryall <mark@ryall.name>
2014-08-26 17:53:12 +1000

    added verbose log mode to see what decisions are being made and also added hack to deal with ci servers that lose last build information while building

--- lib/chicanery/servers.rb
+++ lib/chicanery/servers.rb
@@ -10,38 +10,58 @@ module Chicanery
 
     def check_servers current_state, previous_state
       current_state[:servers] = {}
-      verbose "Execution #{Time.now}"
+      verbose_blue "Execution #{Time.now}"
       servers.each do |server|
         verbose "examining server #{server.name}"
         current_jobs = server.jobs
-        compare_jobs current_jobs, previous_state[:servers][server.name] if previous_state[:servers]
+        if previous_state[:servers] && previous_state[:servers][server.name]
+          compare_jobs current_jobs, previous_state[:servers][server.name]
+        else
+          verbose "\tno past server #{server.name} state for comparison"
+        end
         current_state[:servers][server.name] = current_jobs
       end
     end
 
     def compare_jobs current_jobs, previous_jobs
-      unless previous_jobs
-        verbose "\tno past state for comparison"
-        return
-      end
       current_jobs.each do |job_name, job|
-        verbose "\texamining job #{job_name}"
-        compare_job job_name, job, previous_jobs[job_name] if previous_jobs[job_name]
+        verbose "\texamining job #{job_name} (now #{job[:activity]}, was #{job[:last_build_status]})"
+        if previous_jobs[job_name]
+          compare_job job_name, job, previous_jobs[job_name]
+        else
+          verbose "\tno past job #{job_name} state for comparison"
+        end
       end
     end
 
     def compare_job name, current, previous
-      if current[:activity] == :building and previous[:activity] == :sleeping
-        notify_started_handlers name, current
-      end
-      unless current[:last_build_time] != previous[:last_build_time]
-        verbose "\t\tno change in timestamp"
+      if current[:activity] == :building
+        verbose "\t\tcurrently building"
+        hack_for_forgetful_ci current, previous
+        if previous[:activity] == :sleeping
+          notify_started_handlers name, current
+        end
         return
       end
+      unless current[:last_build_time] != previous[:last_build_time] || current[:last_build_status] != previous[:last_build_status]
+        verbose "\t\tno change in timestamp or build status"
+        return
+      end
+      verbose "\t\t#{previous[:last_build_status]} -> #{current[:last_build_status]}"
       notify_succeeded_handlers name, current if current[:last_build_status] == :success
       notify_failed_handlers name, current if current[:last_build_status] == :failure
-      notify_broken_handlers name, current if current[:last_build_status] == :failure and previous[:last_build_status] == :success
-      notify_fixed_handlers name, current if current[:last_build_status] == :success and previous[:last_build_status] == :failure
+      notify_broken_handlers name, current if current[:last_build_status] == :failure && previous[:last_build_status] == :success
+      notify_fixed_handlers name, current if current[:last_build_status] == :success && previous[:last_build_status] == :failure
+    end
+
+    def hack_for_forgetful_ci current, previous
+      # the following unfortunate hack is to compensate for cctray implementations (semaphore) which lose last build information when building
+      return if current[:last_build_time] > 0
+      verbose "\t\tcopying previous build content"
+      %w(build_time build_status label).each do |property|
+        key = "last_#{property}".to_sym
+        current[key] = previous[key]
+      end
     end
   end
 end
